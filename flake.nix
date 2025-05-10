2a606ed569a6d526e3de6899dcce7a55f47d1367
Ilya Osipov <iljaosipov@gmail.com>
2025-05-10 02:49:19 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,44 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.11"; # Лучше стабильную ревизию
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        devShells.default = pkgs.mkShell.override {
+  outputs = inputs:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override {
+          # Override stdenv in order to change compiler:
           stdenv = pkgs.clangStdenv;
         } {
-          packages = with pkgs; [
-            babashka
-            clang-tools
-            cmake
-            codespell
-            conan
-            cppcheck
-            doxygen
-            gtest
-            lcov
-            vcpkg
-            vcpkg-tool
-          ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          packages = with pkgs;
+            [
+              babashka
+              lua
+              clang-tools
+              cmake
+              codespell
+              conan
+              cppcheck
+              doxygen
+              gtest
+              lcov
+              vcpkg
+              vcpkg-tool
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
 
           shellHook = ''
             echo "🔧 Entering C/C++ dev shell (clang)..."
-            zsh
+            exec zsh
           '';
         };
-      }
-    ) // {
+      });
+
       templates.default = {
         path = ./.;
         description = "C/C++ dev environment with Clang and tools";
